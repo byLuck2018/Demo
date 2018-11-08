0df4aee77f0684f604ffe82b7aa6690aa9f9a7fa
jack <526037856@qq.com>
2018-11-08 15:25:09 +0800

    test

---
+++ Pages/Dtos/GetPageQueryInput.cs
@@ -0,0 +1,86 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services.Dto;
+using Abp.Extensions;
+using Abp.Runtime.Validation;
+
+
+using DMS.Mall.Website;
+
+namespace DMS.Mall.Website.Application
+{
+    public class GetPageQueryInput  : IInputDto, IPagedResultRequest, ISortedResultRequest
+    {
+        //DOTO:在这里增加查询参数
+		[Range(0, 1000)]
+        public int MaxResultCount { get; set; }
+
+        public int SkipCount { get; set; }
+        public int PageType { get; set; }
+        public int StoreId { get; set; }
+        public int Status { get; set; }
+
+        public string Sorting { get; set; }
+
+        public string Keywords { get; set; }
+
+        //public void AddValidationErrors(List<ValidationResult> results)
+        //{
+        //    var validSortingValues = new[] { "CreationTime DESC", "VoteCount DESC", "ViewCount DESC", "AnswerCount DESC" };
+
+        //    if (!Sorting.IsIn(validSortingValues))
+        //    {
+        //        results.Add(new ValidationResult("Sorting is not valid. Valid values: " + string.Join(", ", validSortingValues)));
+        //    }
+        //}
+
+
+
+        /// <summary>
+        /// 页面分类
+        /// </summary>
+        //public Guid? PageCategoryId { get; set; }
+
+
+
+    }
+
+
+    public class GetRecommendQueryInput : IInputDto, IPagedResultRequest
+    {
+        //DOTO:在这里增加查询参数
+        [Range(0, 1000)]
+        public int MaxResultCount { get; set; }
+
+        public int SkipCount { get; set; }
+
+        public string Sorting { get; set; }
+
+        public int Status { get; set; }
+
+        public string CityId { get; set; }
+
+        public string Token { get; set; }
+
+        public int StoreId { get; set; }
+
+    }
+}
---
+++ Pages/Dtos/PageQueryDto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+
+namespace DMS.Mall.Website.Application
+{
+    [AutoMapFrom(typeof(Page))]
+    public class PageQueryDto : EntityDto
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 页面类型(1,登录页2,首页3,详情页4,列表页5,自定义页)
+        /// </summary>
+        public int PageType { get; set; }
+        public string PageTypeName { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// 页面位置(1,电脑端页面2,移动端页面)
+        /// </summary>
+        public int PagePosition { get; set; }
+        public string PagePositionName { get; set; }
+
+        /// <summary>
+        /// 状态(-1,作废1,初始2,已发布)
+        /// </summary>
+        public int Status { get; set; }
+        public string StatusName { get; set; }
+
+        /// <summary>
+        /// 排序
+        /// </summary>
+        public int SortNum { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+        public long? CreatorUserId { get; set; }
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 店铺
+        /// </summary>
+        public virtual string StoreId
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 预览地址
+        /// </summary>
+        public string PreviewUrl
+        {
+            get;
+            set;
+        }
+    }
+}
